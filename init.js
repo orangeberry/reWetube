69faf687842c52513718ccbbc53617fa696a5576
orangeberry <orangeberry@kakao.com>
2020-10-29 00:14:40 +0900

    .

---
+++ init.js
@@ -0,0 +1,7 @@
+import app from "./app";
+
+const PORT = 2000;
+
+const handleListening = () => console.log(`🟠Listening on: http://localhost:${PORT}`)
+
+app.listen(PORT, handleListening)
---
+++ routers/videoRouter.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { deleteVideo, editVideo, upload, videoDetail } from "../controllers/videoController";
+import routes from "../routes";
+
+const videoRouter = express.Router()
+
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
+export default videoRouter
+
