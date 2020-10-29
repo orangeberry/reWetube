e6eb2ccdfd8ddfb5c1934871a0074eea92c155ac
orangeberry <orangeberry@kakao.com>
2020-10-29 23:52:01 +0900

    .

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import morgan from "morgan"
 import helmet from "helmet"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
+import { localsMiddleware } from "./middlewares"
+import routes from "./routes"
 import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
 import globalRouter from "./routers/globalRouter"
-import routes from "./routes"
 
 const app = express()
 
@@ -16,6 +17,8 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', "pug")
+
+app.use(localsMiddleware)
 
 app.use(routes.home, globalRouter)
 app.use(routes.users, userRouter)
