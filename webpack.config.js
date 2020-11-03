4a0fc19b49b1810bca5b6a6c6636b547ec46879d
orangeberry <orangeberry@kakao.com>
2020-11-03 23:18:57 +0900

    .

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const autoprefixer = require("autoprefixer");
 const MiniExtractCSS = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
-const ENTRY_FILE = path.resolve(__dirname, "views", "assets", "js", "main.js");
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
